6d0e42172e4890f23621cfbab29aea67c51b446d
Paulo Andrade <parthur207@gmail.com>
2026-01-11 16:52:24 -0300

    Configuração da classe (Parametros de conexão) que permite o acesso a dados com ADO.NET.

---
+++ TaskManager.Adapters/Persistence/DataBase/Interface/IDbConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Adapters.Persistence.DataBase.Interface
+{
+    public interface IDbConnectionFactory
+    {
+        SqlConnection Create();  
+    }
+}
---
+++ TaskManager.Adapters/Persistence/DataBase/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Adapters.Persistence.DataBase.Interface;
+
+namespace TaskManager.Adapters.Persistence.DataBase
+{
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Default");
+        }
+
+        public SqlConnection Create()
+            => new SqlConnection(_connectionString);
+    }
+}
